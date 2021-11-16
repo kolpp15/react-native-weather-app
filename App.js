10b2ed5f497987400e09cc90eda66fedfb1bbbbb
Brian Sohn <kolpp15@gmail.com>
2021-11-15 18:28:35 -0800

    install expo-location extension

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Alert } from 'react-native';
 import Loading from './Loading';
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <Loading />
-  );
+export default class extends React.Component {
+  
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const location = await Location.getCurrentPositionAsync();
+      console.log('this is the location>>', location);
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad!");
+    }
+  }
+  
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    return <Loading />;
+  }
 }
