0dac630c3c4be48a9d5e0d332e4f56fef8b24c2f
Brian Sohn <kolpp15@gmail.com>
2021-11-30 23:11:26 -0800

    fixed getLocation error. Now loading location

--- App.js
+++ App.js
@@ -1,31 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from "expo-location";
+
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
 
-  getLocation = async() => {
+  getLocation = async () => {
     try {
-      await Location.requestPermissionsAsync();
-      const { 
-        coords: { latitude, longitude } 
+      await Location.requestForegroundPermissionsAsync();
+      const { coords
+        // coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
-      console.log('lat>', coords.latitude, 'long>', coords.longitude);
       this.setState({ isLoading: false });
+      console.log('location>>', coords.latitude, coords.longitude);
     } catch (error) {
-      Alert.alert("Can't find you.", "So sad!");
+      Alert.alert("Can't find you.", "So sad");
     }
-  }
-  
+  };
   componentDidMount() {
     this.getLocation();
   }
-
   render() {
     const { isLoading } = this.state;
     return isLoading ? <Loading /> : null;
