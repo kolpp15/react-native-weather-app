536617ea5f0ea2a473488992feb579f2f5728349
Brian Sohn <kolpp15@gmail.com>
2021-11-30 23:46:08 -0800

    app renders live temperature using the API

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Alert } from 'react-native';
 import Loading from './Loading';
+import Weather from './Weather';
 import * as Location from "expo-location";
 import axios from 'axios';
 
@@ -14,9 +15,10 @@ export default class extends React.Component {
 
   getWeather = async(latitude, longitude) => {
     const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     )
-    console.log('data>', data)
+    // console.log('data>', data)
+    this.setState({ isLoading: false, temp: data.main.temp })
   }
 
   getLocation = async () => {
@@ -36,7 +38,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}/>;
   }
 }
